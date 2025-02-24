5734d5d9ebabd705494ad5e9697959c179d9fa66
Ashis Ranjan Hota <ashishota83@gmail.com>
2025-02-24 20:26:58 +0530

    Added Leetcode #32 Solution

--- Leetcode/32.cpp
+++ Leetcode/32.cpp
@@ -1,35 +1,57 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 class Solution {
-    public:
-        int longestValidParentheses(string s) {
-            if(s.empty()) return 0;
+public:
+    int longestValidParentheses(string s) {
+        int left = 0;
+        int right = 0;
+        int maxi = 0;
 
-            stack<char> st;
-            for(int i=0;i<s.size();i++) {
-                if(s[i] == '(') {
-                    if(st.empty() || st.top() == ')') {
-                        st.push(s[i]);
-                    }
-                    
-                } else {
-                    if(st.top() == '(') {
-                        st.push(s[i]);
-                    }
-                }
-                    
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                left++;
+            } else {
+                right++;
             }
-            return st.size();
-    
+            if (left == right) {
+                maxi = max(maxi, left + right);
+            } else if (right > left) {
+                left = 0;
+                right = 0;
+            }
         }
+
+        left = 0;
+        right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxi = max(maxi, left + right);
+            } else if (left > right) {
+                left = 0;
+                right = 0;
+            }
+        }
+        return maxi;
+    }
 };
 
 int main() {
-    string s = "()(()()())";
-    Solution code;
-    int res = code.longestValidParentheses(s);
-    cout<<res<<endl;
+    Solution solution;
+    string s;
+    
+    cout << "Enter a string of parentheses: ";
+    cin >> s;
+
+    int result = solution.longestValidParentheses(s);
+    cout << "Longest valid parentheses length: " << result << endl;
+
+    return 0;
 }
